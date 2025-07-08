1e29220a4569aafdc13e74034622a69c52ac3447
ZahiraPH <zahirapriyanhusna@gmail.com>
2025-07-08 09:32:36 +0700

    pembaruan config database

--- config/database.php
+++ config/database.php
@@ -1,8 +1,8 @@
 <?php
 $host = 'localhost';
-$dbname = 'fokri_ptk';
-$username = 'root';
-$password = '';
+$dbname = 'projec15_fokri_ptk';
+$username = 'projec15_root';
+$password = '@kaesquare123';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
